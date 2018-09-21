2ad7d9b23e5c31e65d56473f6b9e53dec0ed8def
Alex Schiffman <alexschiff1@gmail.com>
2018-09-21 14:51:51 -0400

    Updated README and bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Fraction-Approximator
+This is a basic algorithm I wrote a couple years ago to convert decimal numbers into fractions.
--- numApprox.py
+++ numApprox.py
@@ -10,30 +10,31 @@ class numApprox:
 		result = self.calc(number)
 
 		if self.calc(number):
-			print self.calc(number)
+			print self.output(self.calc(number), "")
 
 		elif self.calc(number/pi):
-			print self.calc(number/pi), "*pi"
+			print self.output(self.calc(number/pi), "*pi")
+
 		elif self.calc(number*pi):
-			print self.calc(number*pi), "/pi"
+			print self.output(self.calc(number*pi), "/pi")
 
 		elif self.calc(number/rt2):
-			print self.calc(number/rt2), "*root 2"
+			print self.output(self.calc(number/rt2), "*root 2")
 
 		elif self.calc(number/rt3):
-			print self.calc(number/rt3), "*root 3"
-
+			print self.output(self.calc(number/rt3), "*root 3")
 
 		else:
 			print False
 
-	def calc(self, x, error=0.0000000000001):
+
+	def calc(self, x, error=0.00000001):
 		n = int(math.floor(x))
 		x -= n
 		if x < error:
-			return (n)
+			return (n, 1)
 		elif 1 - error < x:
-			return (n+1)
+			return (n+1, 1)
 
 		nLow = 0
 		dLow = 1
@@ -64,9 +65,11 @@ class numApprox:
 		if not solved:
 			return False
 
-	def output(self, result):
+
+	def output(self, result, multiplier):
 		N, D = result
-		return str(N) + " / " + str(D)
+		return str(N) + " / " + str(D) + " " + multiplier
+
 
 if __name__ == "__main__":
 	numApprox().init()
